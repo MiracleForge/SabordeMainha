75e03716472f65f5d6a657abaed94f57071ff1e0
MiracleForge <paulomoreirarosado@hotmail.com>
2024-01-31 12:48:54 -0300

    otimization of useFilter sort logic

--- src/app/hook/useFilter.ts
+++ src/app/hook/useFilter.ts
@@ -11,64 +11,41 @@ const useFilter = ({ setPageNumber, setDisplayedProducts }: UseFilterProps) => {
 
   const handleMenuItemClick = (item: string) => {
     setPageNumber(1);
-
-    setSortOption((prevSortOption) => {
-      if (prevSortOption !== item) {
-        setTimeout(() => {
-          // Close the dropdowns directly in the Filter component
-          // instead of here
-        }, 0);
-      }
-
-      return item;
-    });
+    setSortOption(item);
   };
 
-
   useEffect(() => {
-    let filteredProducts = [...dataProducts];
-    let resultedFilteredProducts = [...filteredProducts];
+    const filterFunctions: Record<string, (product: any) => boolean> = {
+      'Bolos Tradicionais': (product) => product.type === 'Bolos Tradicionais',
+      'Bolos de Aniversário': (product) => product.type === 'Bolos de Aniversário',
+      'Confeitaria': (product) => product.type === 'Confeitaria',
+      'Doces de Festa': (product) => product.type === 'Doces de Festa',
+      'Salgados': (product) => product.type === 'Salgados',
+      'Tortas Salgadas': (product) => product.type === 'Tortas Salgadas',
+      'Salgados de Festas': (product) => product.type === 'Salgados de Festas',
+      'Kits': (product) => product.type === 'Kits',
+    };
 
-    if (sortOption === 'Bolos Tradicionais') {
-        console.log('Filtering by Bolos Tradicionais');
-        resultedFilteredProducts = filteredProducts.filter(product => product.type === 'Bolos Tradicionais');
-      } else if (sortOption === 'Bolos de Aniversário') {
-        console.log('Filtering by Bolos de Aniversário');
-        resultedFilteredProducts = filteredProducts.filter(product => product.type === 'Bolos de Aniversário');
-      } else if (sortOption === 'Confeitaria') {
-        console.log('Filtering by Confeitaria');
-        resultedFilteredProducts = filteredProducts.filter(product => product.type === 'Confeitaria');
-      } else if (sortOption === 'Doces de Festa') {
-        console.log('Filtering by Doces de Festa');
-        resultedFilteredProducts = filteredProducts.filter(product => product.type === 'Doces de Festa');
-      } else if (sortOption === 'Salgados') {
-        console.log('Filtering by Salgados');
-        resultedFilteredProducts = filteredProducts.filter(product => product.type === 'Salgados');
-      } else if (sortOption === 'Tortas Salgadas') {
-        console.log('Filtering by Tortas Salgadas');
-        resultedFilteredProducts = filteredProducts.filter(product => product.type === 'Tortas Salgadas');
-      } else if (sortOption === 'Salgados de Festas') {
-        console.log('Filtering by Salgados de Festas');
-        resultedFilteredProducts = filteredProducts.filter(product => product.type === 'Salgados de Festas');
-      } else if (sortOption === 'Kits') {
-        console.log('Filtering by Kits');
-        resultedFilteredProducts = filteredProducts.filter(product => product.type === 'Kits');
-      }
+    const sortingFunctions: Record<string, (a: any, b: any) => number | any> = {
+      'Menor Preço': (a, b) => a.cost - b.cost,
+      'Maior Preço': (a, b) => b.cost - a.cost,
+      'Ordem Alphabética': (a, b) => a.name.localeCompare(b.name),
+    };
 
-    // Sorting conditions
-    if (sortOption === 'Menor Preço') {
-        console.log('Sorting by Menor Preço');
-        resultedFilteredProducts.sort((a, b) => a.cost - b.cost);
-      } else if (sortOption === 'Maior Preço') {
-        console.log('Sorting by Maior Preço');
-        resultedFilteredProducts.sort((a, b) => b.cost - a.cost);
-      } else if (sortOption === 'Ordem Alphabética') {
-        console.log('Sorting by Ordem Alphabética');
-        resultedFilteredProducts.sort((a, b) => a.name.localeCompare(b.name));
-      }
+    let resultedFilteredProducts = [...dataProducts];
+
+    if (filterFunctions[sortOption]) {
+      //console.log(`Filtering by ${sortOption}`);
+      resultedFilteredProducts = resultedFilteredProducts.filter(filterFunctions[sortOption]);
+    }
+
+    if (sortingFunctions[sortOption]) {
+      //console.log(`Sorting by ${sortOption}`);
+      resultedFilteredProducts.sort(sortingFunctions[sortOption]);
+    }
 
     setDisplayedProducts(resultedFilteredProducts);
-  }, [sortOption, setDisplayedProducts]);
+  }, [sortOption, setDisplayedProducts, setPageNumber]);
 
   return {
     handleMenuItemClick,
